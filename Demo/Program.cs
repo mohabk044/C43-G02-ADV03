788ede91894215be6b85b3270627a68842d1c7d9
Mohab Khaled <mohabk044@gmail.com>
2025-01-21 10:31:33 +0200

    LinkedList

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Generic Collection - LinkedList
+            LinkedList<int> Numbers = new LinkedList<int>();
+            Numbers.AddFirst(1);
+            Numbers.AddAfter(Numbers.First, 2);
+            Numbers.AddAfter(Numbers.First.Next, 3);
+            Numbers.AddLast(4);
+            foreach (int number in Numbers)
+            {
+                Console.WriteLine(number);
+            }
+            #endregion
         }
     }
 }
